ef4b235a5869166d1373e141b7d59d38937f7821
陈岳军 <chenyuejun@eastmoney.com>
2023-11-11 16:42:30 +0800

    file upload

---
+++ File.java
@@ -0,0 +1,16 @@
+import java.io.*;  
+  
+public class File {  
+    public static void main(String[] args) {  
+        try {  
+            FileWriter fw = new FileWriter("filename.csv", true); // 第二个参数为true表示追加内容
+            BufferedWriter bw = new BufferedWriter(fw);  
+            bw.write("column1,column2,column3");
+            bw.newLine(); // 可选操作，在文本末尾添加一个新行  
+            bw.close();  
+            System.out.println("Content appended to file successfully.");  
+        } catch (IOException e) {  
+            e.printStackTrace();  
+        }  
+    }  
+}
